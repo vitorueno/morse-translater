36c73d9d9156fe2d9795da69e8ae669df4764dbc
Vítor Augusto Ueno Otto <vitoruenootto@gmail.com>
2020-05-26 12:54:49 -0300

    update .gitignore to ignore the pytest cache

--- morse/tests/test_encrypt_decrypt.py
+++ morse/tests/test_encrypt_decrypt.py
@@ -4,5 +4,3 @@ from ..decrypt import decrypt
 def test_encrypt_decrypt():
     assert decrypt(encrypt('OI')) == 'OI '
     assert decrypt(encrypt('PYTHON-PROGRAM')) == 'PYTHON-PROGRAM '
-    mes = encrypt('oi pessoal')
-    assert decrypt(mes.rstrip()) == 'OI PESSOAL'
